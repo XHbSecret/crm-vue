91b76a285db23f4a46b372a94f0134e04b8f1643
zhangzhiyunzzy <2567087117@qq.com>
2022-05-31 09:22:27 +0800

    index文件

--- src/api/index.js
+++ src/api/index.js
@@ -8,17 +8,14 @@ import * as role from './system/role'
 import * as product from './system/product'
 import * as contract from './contract/index'
 import * as checkFlow from './check/checkFlow'
-<<<<<<< HEAD
 import * as echartss from './echartss/index'
 import * as sales from './sales/index'
-=======
-<<<<<<< HEAD
+
 import * as customerContract from'./contract/contract'
-=======
+
 import * as backMoney from './back/backMoney'
 
->>>>>>> 344e1a19a775cfa584fde5afe20f4a9ea592c637
->>>>>>> ff908d6a91d922ab54253f3495f79a186a817ad6
+
 export default {
     activity,
     login,
@@ -29,14 +26,12 @@ export default {
     product,
     contract,
     checkFlow,
-<<<<<<< HEAD
+
     echartss,
-    sales
-=======
-<<<<<<< HEAD
+    sales,
+
     customerContract,
-=======
+
     backMoney
->>>>>>> 344e1a19a775cfa584fde5afe20f4a9ea592c637
->>>>>>> ff908d6a91d922ab54253f3495f79a186a817ad6
+
 }
